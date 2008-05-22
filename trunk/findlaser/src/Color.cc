539e98549d718b8f79c23e2d3c0319998613ebfa
tsee <tsee@45db8e28-7f79-11dd-8a74-1916a93a1be7>
2008-05-22 16:26:42 +0000

    switch h and cc, oops

--- trunk/findlaser/src/Color.cc
+++ trunk/findlaser/src/Color.cc
@@ -1,18 +1,17 @@
 #include "Color.h"
+#include <cmath>
 
 namespace FindLaser {
- // I know, public... but this is just a struct!
-  class Color {
-    public:
-    Color(unsigned char r, unsigned char g, unsigned char b);
 
-    ~Color() {}
-
-    Color GetNormalized();
-
-    unsigned char red;
-    unsigned char green;
-    unsigned char blue;
-  }; // end class Color
+  Color::Color(unsigned char r, unsigned char g, unsigned char b) : red(r), green(g), blue(b) {
+  }
+   
+  Color Color::GetNormalized() {
+    float scale = 255. / pow((float) (red*red+green*green+blue*blue), 0.5);
+    float r = (float)red   * scale;
+    float g = (float)green * scale;
+    float b = (float)blue  * scale;
+    Color col((unsigned char)r, (unsigned char)g, (unsigned char)b);
+    return col;
+  }
 } // end namespace FindLaser
-
