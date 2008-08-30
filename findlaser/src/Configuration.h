f078fb3835b16195618e47d91a50e803c8834610
tsee <tsee@45db8e28-7f79-11dd-8a74-1916a93a1be7>
2008-08-30 21:03:44 +0000

    r833@l3tsee:  tsee | 2008-08-12 12:51:48 +0200  highly experimental, add constraints to brightness, contrast, and the two thresholds

--- findlaser/src/Configuration.h
+++ findlaser/src/Configuration.h
@@ -19,14 +19,14 @@ namespace FindLaser {
       void SetCameraImageSizeY(const unsigned int sizeY) { fCameraImageSizeY = sizeY; }
 
       int GetCameraBrightness() { return fCameraBrightness; }
-      void SetCameraBrightness(const int camBr) { fCameraBrightness = camBr; }
+      void SetCameraBrightness(const int camBr);
       int GetCameraContrast() { return fCameraContrast; }
-      void SetCameraContrast(const int camCo) { fCameraContrast = camCo; }
+      void SetCameraContrast(const int camCo);
 
       double GetBrightnessThreshold() { return fBrightnessThreshold; }
-      void SetBrightnessThreshold(const double briThr) { fBrightnessThreshold = briThr; }
+      void SetBrightnessThreshold(const double briThr);
       double GetColorThreshold() { return fColorThreshold; }
-      void SetColorThreshold(const double colThr) { fColorThreshold = colThr; }
+      void SetColorThreshold(const double colThr);
 
       const std::string& GetCameraDevice() { return fCameraDevice; }
       void SetCameraDevice(const std::string& dev) { fCameraDevice = dev; }
@@ -37,8 +37,27 @@ namespace FindLaser {
       GeometryCorrection* GetGeometryCorrection() { return fGeometryCorrection; }
       void SetGeometryCorrection(GeometryCorrection* g);
 
+      int GetCameraBrightnessMin() { return fgCameraBrightnessMin; }
+      int GetCameraBrightnessMax() { return fgCameraBrightnessMax; }
+      int GetCameraContrastMin() { return fgCameraContrastMin; }
+      int GetCameraContrastMax() { return fgCameraContrastMax; }
+
+      int GetBrightnessThresholdMin() { return fgBrightnessThresholdMin; }
+      int GetBrightnessThresholdMax() { return fgBrightnessThresholdMax; }
+      int GetColorThresholdMin() { return fgColorThresholdMin; }
+      int GetColorThresholdMax() { return fgColorThresholdMax; }
+
     private:
       bool ReadConfiguration();
+
+      const static unsigned int fgCameraBrightnessMin;
+      const static unsigned int fgCameraBrightnessMax;
+      const static unsigned int fgCameraContrastMin;
+      const static unsigned int fgCameraContrastMax;
+      const static unsigned int fgBrightnessThresholdMin;
+      const static unsigned int fgBrightnessThresholdMax;
+      const static unsigned int fgColorThresholdMin;
+      const static unsigned int fgColorThresholdMax;
 
       std::string fFileName;
       unsigned int fCameraImageSizeX;
