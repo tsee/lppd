8b15e9da08baecea9f71c8af19af15af523d7084
smueller@cpan.org <smueller@cpan.org@45db8e28-7f79-11dd-8a74-1916a93a1be7>
2008-09-13 15:27:38 +0000

    history point needs to know whether it's triggered or not

--- trunk/findlaser/src/HistoryPoint.cc
+++ trunk/findlaser/src/HistoryPoint.cc
@@ -7,10 +7,9 @@ using namespace std;
 
 namespace FindLaser {
 
-  HistoryPoint::HistoryPoint(const clock_t t, const double x, const double y) {
-    fTime = t;
-    fX = x;
-    fY = y;
+  HistoryPoint::HistoryPoint(const clock_t t, const double x, const double y, const bool triggered)
+    : fTime(t), fX(x), fY(y), fTrigger(triggered)
+  {
   }
 
 } // end namespace FindLaser
--- trunk/findlaser/src/HistoryPoint.h
+++ trunk/findlaser/src/HistoryPoint.h
@@ -7,7 +7,7 @@ namespace FindLaser {
 
   class HistoryPoint {
     public:
-      HistoryPoint(const clock_t t, const double x, const double y);
+      HistoryPoint(const clock_t t, const double x, const double y, bool triggered);
 
       double GetX() { return fX; }
       void SetX(const double x) { fX = x; }
@@ -15,11 +15,15 @@ namespace FindLaser {
       void SetY(const double y) { fY = y; }
       clock_t GetT() { return fTime; }
       void SetT(const clock_t t) { fTime = t; }
+
+      bool Triggered() { return fTrigger; }
+      void SetTrigger(const bool triggered) { fTrigger = triggered; }
       
     private:
       clock_t fTime;
       double fX;
       double fY;
+      bool fTrigger;
   }; // end class HistoryPoint
 
 } // end namespace FindLaser
