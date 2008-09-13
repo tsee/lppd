931a717d4e3956ca0681a39a2e95902fd4cca162
smueller@cpan.org <smueller@cpan.org@45db8e28-7f79-11dd-8a74-1916a93a1be7>
2008-09-13 15:29:10 +0000

    add Events and EventTypes

---
+++ findlaser/src/EventType.cc
@@ -0,0 +1,21 @@
+
+#include "EventType.h"
+
+#include <string>
+#include <iostream>
+
+#include "HistoryPoint.h"
+
+using namespace std;
+
+namespace FindLaser {
+
+  EventType::EventType()
+  {
+  }
+
+  vector<Event> EventType::Find(const list<HistoryPoint>& points) {
+    // FIXME do something
+  }
+
+} // end namespace FindLaser
